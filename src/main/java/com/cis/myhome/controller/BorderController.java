7cad580aa5a0f9d197a89f298c716df9b7105d19
JeongHun Park <netwo115@naver.com>
2021-02-06 20:51:05 +0900

    first commit

--- src/main/java/com/cis/myhome/controller/BorderController.java
+++ src/main/java/com/cis/myhome/controller/BorderController.java
@@ -1,15 +1,28 @@
 package com.cis.myhome.controller;
 
+import com.cis.myhome.model.Board;
+import com.cis.myhome.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.cis.myhome.controller.BorderController;
+
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/board")
 public class BorderController {
 
+    @Autowired
+    private BoardRepository boardRepository;
+
     @GetMapping("/list")
-    public String list() {
+    public String list(Model model) {
+        List<Board> boards = boardRepository.findAll();
+        model.addAttribute("boards", boards);
         return "board/list";
     }
 
