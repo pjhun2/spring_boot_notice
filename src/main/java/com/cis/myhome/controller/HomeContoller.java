db6ceb4135a33140f57dc6d1590f0ee7222adbb3
JeongHun Park <netwo115@naver.com>
2021-02-06 13:22:32 +0900

    init

---
+++ src/main/java/com/cis/myhome/controller/HomeContoller.java
@@ -0,0 +1,14 @@
+package com.cis.myhome.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeContoller {
+
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+
+}
