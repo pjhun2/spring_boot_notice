29c8c0097fe2887ba7e08e9e598567a5b3fde773
JeongHun Park <netwo115@naver.com>
2021-02-07 13:00:28 +0900

    commit

--- src/main/java/com/cis/myhome/controller/BorderController.java
+++ src/main/java/com/cis/myhome/controller/BorderController.java
@@ -5,8 +5,7 @@ import com.cis.myhome.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import com.cis.myhome.controller.BorderController;
 
 
@@ -24,6 +23,24 @@ public class BorderController {
         List<Board> boards = boardRepository.findAll();
         model.addAttribute("boards", boards);
         return "board/list";
+    }
+
+    @GetMapping("/form")
+    public String form(Model model, @RequestParam(required = false) Long id) {
+        if(id == null) {
+            model.addAttribute("board", new Board());
+        } else {
+            Board board = boardRepository.findById(id).orElse(null);
+            model.addAttribute("board", board);
+        }
+
+        return "board/form";
+    }
+
+    @PostMapping("/form")
+    public String greetingSubmit(@ModelAttribute Board board) {
+        boardRepository.save(board);
+        return "redirect:/board/list";
     }
 
 }
